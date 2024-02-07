1f2c52e7354ac3a57de1de63f01c58ee620dfd80
guillovilla <136508660+guillovilla@users.noreply.github.com>
2024-02-07 14:20:49 -0500

    /api/films/:id ok

--- app.js
+++ app.js
@@ -23,7 +23,48 @@ server.use(express.static(path.join(__dirname, "public")));
 //Permet d'accepter des body en Json dans les requetes
 server.use(express.json()); 
 
-server.post("/donnees/initialiser", (req, res) =>{
+
+server.get("/api/films", async (req, res) => {
+    try{
+            const direction = req.query["order-direction"] || "asc";
+            const propriete = req.params.propriete;
+            const donneesRef = await db.collection("films").orderBy(propriete, direction).tri("propriete").get();
+            const donneesFinale = [];
+
+            donneesRef.forEach((doc) =>{
+                donneesFinale.push(doc.data());
+
+            });
+
+            res.statusCode = 200;
+            res.json(donneesFinale);
+    } catch (erreur) {
+            res.statusCode = 500;
+            res.json({message: "Une erreur est survenue"});
+    }
+});
+
+
+server.get("/api/films/:id", async (req, res)=>{    
+  
+    const id = req.params.id;
+    const resultat = await db.collection("films").doc(id).get();
+
+   if(resultat) {
+
+    res.statusCode = 200;
+    res.json(resultat.data());
+
+   }else{
+
+    res.statusCode = 404;
+    res.json({message: 'film non trouvee'});
+
+   }
+});
+
+
+server.post("/api/films/initialiser", (req, res) =>{
 
     const donneesTest = require("./data/donneesTest/filmsTest.js");
 
